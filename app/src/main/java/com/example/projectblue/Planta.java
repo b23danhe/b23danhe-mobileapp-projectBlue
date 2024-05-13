2dc2756b1e5b41ac3018d9e0dc2ce2aaf6a8bd67
b23danhe <b23danhe@student.his.se>
2024-05-13 10:19:03 +0200

    Add Planta class

---
+++ app/src/main/java/com/example/projectblue/Planta.java
@@ -0,0 +1,17 @@
+package com.example.projectblue;
+
+public class Planta {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private int Size;
+    private int cost;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
